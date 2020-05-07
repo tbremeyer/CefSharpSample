99dd98daf7dfdca705d47b1e873993424a72fbaa
Bremeyer, Torsten <Torsten.Bremeyer@unisys.com>
2020-05-07 16:29:16 +0200

    Solved Loading of Sample Web Site

--- CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
+++ CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace CefSharp.MinimalExample.Wpf
 {
     public partial class MainWindow : Window
     {
+        private bool _firstTime = true;
         public MainWindow()
         {
             InitializeComponent();
@@ -18,7 +21,23 @@ namespace CefSharp.MinimalExample.Wpf
 
         private async Task CallWebSite()
         {
+            if (!_firstTime) return;
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            var resourcePath = "CefSharp.MinimalExample.Wpf" + ".web.index.html";
+
+            if (executingAssembly.GetManifestResourceInfo(resourcePath) != null)
+            {
+                var resourceStream = executingAssembly.GetManifestResourceStream(resourcePath);
+                var memoryStream = new MemoryStream();
+                await resourceStream.CopyToAsync(memoryStream);
+                resourceStream.Close();
+                memoryStream.Position = 0;
+                var fileText = await new StreamReader(memoryStream).ReadToEndAsync();
+                Browser.LoadHtml(fileText, true);
+            }
+
             var result = await Browser.EvaluateScriptAsync($"httpGet('http://httpbin.org/basic-auth/undefined/undefined?accept=json')");
+            _firstTime = false;
         }
     }
 }
